9e907d926144f330e7050a3374e2704d28c58e70
nikipiki <chendev2003@gmail.com>
2026-04-16 14:52:37 +0300

    Add Yandex Webmaster verification meta tag to Next.js layout

--- app-klub/src/app/layout.tsx
+++ app-klub/src/app/layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ru">
       <head>
+        <meta name="yandex-verification" content="61fe933fed3dbbb8" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
